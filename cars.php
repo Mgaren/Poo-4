e809b43992609082992c61405d6faaafc992c317
Mgaren <basketeur009@gmail.com>
2020-10-25 19:27:30 +0100

    Quete POO4 debut

---
+++ cars.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'vehicule.php';
+
+class Car extends Vehicle
+{
+    /*
+    const ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+    */
+
+    /**
+     * @var string
+     */
+    private $energy;
+
+    /**
+     * @var int
+     */
+    private $energyLevel;
+
+    /**
+     * @var bool
+     */
+    private $hasParkBrake;
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->energy = $energy;
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Car
+    {
+        $this->energy = $energy;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+
+    public function setParkBrake()
+}
